451923133e89692ba3c838f3e92a5fba12b47e3a
qianfeng <qianfeng@qianfengdemac-mini-12.local>
2015-11-23 14:32:59 +0800

    verson1

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright © 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
